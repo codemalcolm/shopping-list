86d700d5c443750bbbf971cf85cf0ee0cd0bec2f
Vojta Cvrcheck <vojta.cvrcek1@gmail.com>
2024-11-04 20:03:20 +0100

    add list modal & fetching users

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,47 @@
-import { Box, Flex, Image, Link, Text, Button } from "@chakra-ui/react";
+import {
+	Box,
+	Flex,
+	Image,
+	Link,
+	Text,
+	Button,
+	Input,
+	Collapsible,
+} from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import { Checkbox } from "./components/ui/checkbox";
 import deleteIcon from "./assets/icons/delete-icon.svg";
 import editIcon from "./assets/icons/edit-icon.svg";
 import saveIcon from "./assets/icons/save-icon.svg";
 import showIcon from "./assets/icons/show-icon.svg";
+import addUserIcon from "./assets/icons/add-user-icon.svg";
+import addIcon from "./assets/icons/add-icon.svg";
 import {
 	MenuContent,
 	MenuItem,
 	MenuRoot,
 	MenuTrigger,
 } from "./components/ui/menu";
+import {
+	DialogRoot,
+	DialogActionTrigger,
+	DialogBody,
+	DialogCloseTrigger,
+	DialogContent,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "./components/ui/dialog";
+import { Field } from "./components/ui/field";
+import { Avatar } from "./components/ui/avatar";
 
 const App = () => {
+	const [inputs, setInputs] = useState({
+		listName: "",
+		membersList: [],
+	});
+	const [fetchUsers, setFetchUsers ] = useState(false)
 	const [loggedUser, setlLoggedUser] = useState();
 	const [itemList, setItemList] = useState([
 		{
@@ -47,6 +76,14 @@ const App = () => {
 			memberList: [],
 			isDone: true,
 		},
+		{
+			id: "td05",
+			name: "Pátý úkolovník",
+			state: "archived",
+			owner: "u2",
+			memberList: ["u1"],
+			isDone: true,
+		},
 	]);
 
 	const userList = [
@@ -61,9 +98,33 @@ const App = () => {
 		console.log("heree");
 	};
 
-	useEffect(()=>{
+	useEffect(() => {
 		setlLoggedUser(userList[0]);
-	},[])
+	}, []);
+
+	// add shopping list
+	const handleAddShoppingList = (inputs) => {
+		if (inputs) {
+			const newItemList = {
+				id: Math.random().toString(36).substr(2, 9), // Random id
+				name: inputs.listName,
+				state: "active",
+				owner: loggedUser.id,
+				memberList: inputs.membersList,
+				isDone: false,
+			};
+
+			setItemList((prevItemLists) => [...prevItemLists, newItemList]);
+			console.log(newItemList);
+			return;
+		}
+		console.log("Something went wrong");
+	};
+
+	// add user
+	const handleAddUser = () => {
+		setFetchUsers(!fetchUsers)
+	};
 
 	return (
 		<>
@@ -95,14 +156,115 @@ const App = () => {
 
 			{/* Flex */}
 			<Flex gap={8} flexDirection={"column"}>
-				{itemList.map((item) => (
+				<Flex
+					justifyContent={"space-between"}
+					alignItems={"center"}
+					px={"16px"}
+				>
+					<Text fontSize={"18px"} fontWeight={500}>
+						Shopping lists :
+					</Text>
+					<Flex gap={2}>
+						<Button borderRadius={"16px"}>Filter</Button>
+						<DialogRoot>
+							<DialogTrigger asChild>
+								<Button borderRadius={"16px"} px={"32px"}>
+									Add shopping list
+								</Button>
+							</DialogTrigger>
+
+							<DialogContent>
+								<DialogHeader>
+									<DialogTitle>Create a Shopping list</DialogTitle>
+								</DialogHeader>
+								<DialogBody>
+									<Flex gap={4} flexDirection={"column"} alignItems={"center"}>
+										<Field label="Name" required>
+											<Input
+												placeholder="Enter shopping list name"
+												onChange={(e) =>
+													setInputs({ ...inputs, listName: e.target.value })
+												}
+											/>
+										</Field>
+										<Collapsible.Root style={{ width: "100%" }}>
+											<Flex flexDirection="column" alignItems="center" w="100%">
+												<Collapsible.Trigger>
+													<Button
+														w={"375px"}
+														onClick={() => {
+															handleAddUser();
+														}}
+													>
+														Add user{" "}
+														<Image w="22px" h="22px" src={addUserIcon} />
+													</Button>
+												</Collapsible.Trigger>
+
+												<Collapsible.Content
+													style={{ display: "flex", justifyContent: "center", width:"100%", height:"100%" }}
+												>
+													<Flex
+														padding="4"
+														borderWidth="1px"
+														w="375px"
+														mt="4"
+														justifyContent="center"
+														flexDirection={"column"}
+														gap={2}
+													>
+													{fetchUsers && 
+													userList.map((user) =>(
+														<Flex key={user.id} justifyContent={"space-between"}>
+															<Flex gap={4} alignItems={"center"}>
+															<Avatar src={user.profilePicUrl} name={user.name} />
+															<Text>{user.name}</Text>
+															</Flex>
+															<Button
+															bgColor={
+																// isAdded ? "red.500" : 
+																"green.500"}
+															onClick={() => {
+																handleAddUser(user.id);
+															}}
+															>
+															{"Add User"}
+															</Button>
+
+														</Flex>
+													))}
+													</Flex>
+												</Collapsible.Content>
+											</Flex>
+										</Collapsible.Root>
+										<Button w={"80%"}>
+											Add items <Image w="25px" h="25px" src={addIcon} />
+										</Button>
+									</Flex>
+								</DialogBody>
+								<DialogFooter>
+									<DialogActionTrigger asChild>
+										<Button variant="outline">Cancel</Button>
+									</DialogActionTrigger>
+									<DialogActionTrigger asChild>
+										<Button onClick={() => handleAddShoppingList(inputs)}>
+											Add Shopping List
+										</Button>
+									</DialogActionTrigger>
+								</DialogFooter>
+								<DialogCloseTrigger />
+							</DialogContent>
+						</DialogRoot>
+					</Flex>
+				</Flex>
+				{itemList?.map((item) => (
 					<Box key={item.id} border={"1px solid black"} px={"32px"} py={"32px"}>
 						<Flex justifyContent={"space-between"} alignItems={"center"}>
 							<Flex gap={4} alignItems={"center"}>
 								<Checkbox
 									size="md"
 									colorPalette="green"
-									defaultChecked={item.isDone}
+									checked={item.isDone}
 								/>
 								<Text>{item.name}</Text>
 							</Flex>
