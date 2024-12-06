3ce079c52b811b9dc5ed14c401ef1afe334529f6
Vojta Cvrcheck <vojta.cvrcek1@gmail.com>
2024-12-06 09:42:28 +0100

    Adde darkmode to mainpage

--- src/App.jsx
+++ src/App.jsx
@@ -37,23 +37,23 @@ import { Field } from "./components/ui/field";
 import { Avatar } from "./components/ui/avatar";
 import { useNavigate } from "react-router";
 import { useShoppingList } from "./context/ShoppingListContext";
+import { Bookmark, CirclePlus, Eye, Trash2 } from "lucide-react";
 
 const App = () => {
-	const navigate = useNavigate()
-	const { shoppingList,setShoppingList, userList } = useShoppingList();
+	const navigate = useNavigate();
+	const { shoppingList, setShoppingList, userList } = useShoppingList();
 	const [showArchived, setShowArchived] = useState(false);
-	
 
 	const [inputs, setInputs] = useState({
 		listName: "",
 		membersList: [],
-		itemList:[]
+		itemList: [],
 	});
 
-	const [itemsFromInputs, setItemsFromInputs] = useState([])
+	const [itemsFromInputs, setItemsFromInputs] = useState([]);
 
-	const [addItem, setAddItem] = useState(false)
-	const [fetchUsers, setFetchUsers ] = useState(false)
+	const [addItem, setAddItem] = useState(false);
+	const [fetchUsers, setFetchUsers] = useState(false);
 	const [loggedUser, setlLoggedUser] = useState();
 
 	// Testing purposes
@@ -67,7 +67,6 @@ const App = () => {
 
 	// add shopping list
 	const handleAddShoppingList = (inputs, itemFromInputs) => {
-
 		if (inputs.listName !== "") {
 			const newShoppingList = {
 				id: Math.random().toString(36).substr(2, 9), // Random id
@@ -75,21 +74,20 @@ const App = () => {
 				state: "active",
 				owner: loggedUser.id,
 				memberList: inputs.membersList,
-				itemList:[],
+				itemList: [],
 				isDone: false,
 			};
 
-			if(itemFromInputs){
-				itemFromInputs.forEach(item => {
+			if (itemFromInputs) {
+				itemFromInputs.forEach((item) => {
 					const newItem = {
 						id: item.id,
 						name: item.itemName,
 						quantity: item.quantity,
 						isDone: false,
-					}
+					};
 					newShoppingList.itemList.push(newItem);
 				});
-				
 			}
 			setShoppingList((prevItemLists) => [...prevItemLists, newShoppingList]);
 
@@ -98,10 +96,10 @@ const App = () => {
 	};
 
 	// add item
-	const handleAddItem = (itemId) =>{
-		const item = inputs.itemList.find((item) => item.id === itemId)
-		const itemName = item ? item.itemName : null
-		const itemQuantity = item.quantity
+	const handleAddItem = (itemId) => {
+		const item = inputs.itemList.find((item) => item.id === itemId);
+		const itemName = item ? item.itemName : null;
+		const itemQuantity = item.quantity;
 
 		const newItem = {
 			id: itemId,
@@ -109,34 +107,33 @@ const App = () => {
 			quantity: itemQuantity,
 			isDone: false,
 			// append item to items array that will be later pushed into itemList array in shoppingList
-		}
-		setItemsFromInputs((prevItems) => [...prevItems, newItem])
-	}
+		};
+		setItemsFromInputs((prevItems) => [...prevItems, newItem]);
+	};
 
 	// add user
 	const handleAddUser = (userId) => {
-		setInputs(prevInputs =>({
+		setInputs((prevInputs) => ({
 			...prevInputs,
-			membersList: [...prevInputs.membersList, userId]
-		}))
+			membersList: [...prevInputs.membersList, userId],
+		}));
 	};
 
-	// add field input 
-	const handleAddInputFields = () =>{
+	// add field input
+	const handleAddInputFields = () => {
 		const newInputField = {
 			id: Math.random().toString(36).substr(2, 9), // Random id
 			itemName: "",
 			quantity: "",
-		}
-		
-		setInputs(prevInputs =>({
+		};
+
+		setInputs((prevInputs) => ({
 			...prevInputs,
-			itemList: [...prevInputs.itemList, newInputField]
-		}))
+			itemList: [...prevInputs.itemList, newInputField],
+		}));
+	};
 
-	}
-
-	// input change of itemList in inputs 
+	// input change of itemList in inputs
 	const handleInputChange = (id, field, value) => {
 		setInputs((prevInputs) => ({
 			...prevInputs,
@@ -144,20 +141,18 @@ const App = () => {
 				item.id === id ? { ...item, [field]: value } : item
 			),
 		}));
-
 	};
-	console.log(inputs)
-
+	console.log(inputs);
 
 	// delete list - done
 	const handleDeleteList = (listId) => {
-		const deleteList = confirm("Do you want to delete this shopping list ?")
-		if(deleteList){
-			setShoppingList(prevItemList => prevItemList.filter((item) => item.id !== listId))
-
+		const deleteList = confirm("Do you want to delete this shopping list ?");
+		if (deleteList) {
+			setShoppingList((prevItemList) =>
+				prevItemList.filter((item) => item.id !== listId)
+			);
 		}
-
-	}
+	};
 
 	// Navigate to list detail
 	const handleShowListDetail = (id) => {
@@ -170,22 +165,22 @@ const App = () => {
 
 	const handleArchiveList = (listId) => {
 		setShoppingList((prevLists) =>
-		  prevLists.map((list) =>
-			list.id === listId
-			  ? { ...list, isArchived: !list.isArchived } // Toggle the list's `isArchived` property
-			  : list
-		  )
+			prevLists.map((list) =>
+				list.id === listId
+					? { ...list, isArchived: !list.isArchived } // Toggle the list's `isArchived` property
+					: list
+			)
 		);
 	};
 
-	const resetInputs = () =>{
+	const resetInputs = () => {
 		setInputs({
 			listName: "",
 			membersList: [],
-			itemList:[]
-		})
-		setItemsFromInputs([])
-	}
+			itemList: [],
+		});
+		setItemsFromInputs([]);
+	};
 
 	return (
 		<>
@@ -237,9 +232,20 @@ const App = () => {
 						>
 							{showArchived ? "Hide archived" : "Show archived"}
 						</Button>
-						<DialogRoot onOpenChange={() => {setFetchUsers(false), setAddItem(false)}} onExitComplete={() => resetInputs()}>
+						<DialogRoot
+							onOpenChange={() => {
+								setFetchUsers(false), setAddItem(false);
+							}}
+							onExitComplete={() => resetInputs()}
+						>
 							<DialogTrigger asChild>
-								<Button borderRadius={"16px"} px={"32px"}>
+								<Button
+									borderRadius={"16px"}
+									px={"32px"}
+									bgColor={{ base: "gray.900" }}
+									color={{ base: "white" }}
+									border={{ base: "1px solid black", _dark: "1px solid white" }}
+								>
 									Add shopping list
 								</Button>
 							</DialogTrigger>
@@ -266,7 +272,7 @@ const App = () => {
 													<Button
 														w={"375px"}
 														onClick={() => {
-															setFetchUsers(!fetchUsers)
+															setFetchUsers(!fetchUsers);
 														}}
 													>
 														Add user
@@ -275,7 +281,12 @@ const App = () => {
 												</Collapsible.Trigger>
 
 												<Collapsible.Content
-													style={{ display: "flex", justifyContent: "center", width:"100%", height:"100%" }}
+													style={{
+														display: "flex",
+														justifyContent: "center",
+														width: "100%",
+														height: "100%",
+													}}
 												>
 													<Flex
 														padding="4"
@@ -286,28 +297,37 @@ const App = () => {
 														flexDirection={"column"}
 														gap={2}
 													>
-													{fetchUsers && 
-													userList.map((user) =>{
-														const isAdded = inputs.membersList.includes(user.id)
-														if(user.id === loggedUser.id) return null // Logged user not showing in the list
-														return(
-															<Flex key={user.id} justifyContent={"space-between"}>
-																<Flex gap={4} alignItems={"center"}>
-																<Avatar src={user.profilePicUrl} name={user.name} />
-																<Text>{user.name}</Text>
-																</Flex>
-																{/* Adds user */}
-																<Button
-																disabled={ isAdded ? true : false }
-																bgColor={ isAdded ? "green.500" : "none"}
-																onClick={() => { handleAddUser(user.id); }}
-																>
-																{isAdded ? "User Added" : "Add User"}
-																</Button>
-
-															</Flex>
-														)
-													})}
+														{fetchUsers &&
+															userList.map((user) => {
+																const isAdded = inputs.membersList.includes(
+																	user.id
+																);
+																if (user.id === loggedUser.id) return null; // Logged user not showing in the list
+																return (
+																	<Flex
+																		key={user.id}
+																		justifyContent={"space-between"}
+																	>
+																		<Flex gap={4} alignItems={"center"}>
+																			<Avatar
+																				src={user.profilePicUrl}
+																				name={user.name}
+																			/>
+																			<Text>{user.name}</Text>
+																		</Flex>
+																		{/* Adds user */}
+																		<Button
+																			disabled={isAdded ? true : false}
+																			bgColor={isAdded ? "green.500" : "none"}
+																			onClick={() => {
+																				handleAddUser(user.id);
+																			}}
+																		>
+																			{isAdded ? "User Added" : "Add User"}
+																		</Button>
+																	</Flex>
+																);
+															})}
 													</Flex>
 												</Collapsible.Content>
 											</Flex>
@@ -315,13 +335,18 @@ const App = () => {
 										<Collapsible.Root style={{ width: "100%" }}>
 											<Flex flexDirection="column" alignItems="center" w="100%">
 												<Collapsible.Trigger>
-												<Button w={"375px"}>
-													Add items <Image w="25px" h="25px" src={addIcon} />
-												</Button>
+													<Button w={"375px"}>
+														Add items <CirclePlus />
+													</Button>
 												</Collapsible.Trigger>
 
 												<Collapsible.Content
-													style={{ display: "flex", justifyContent: "center", width:"100%", height:"100%" }}
+													style={{
+														display: "flex",
+														justifyContent: "center",
+														width: "100%",
+														height: "100%",
+													}}
 												>
 													<Flex
 														padding="4"
@@ -333,38 +358,55 @@ const App = () => {
 														flexDirection={"column"}
 														gap={2}
 													>
-													<Button borderRadius="full" p={0} onClick={() => {handleAddInputFields()}}><Image w="25px" h="25px" src={addIcon}/></Button>
-													{inputs?.itemList?.map((item)=>(
-														<Flex gap={4} alignItems={"end"} key={item.id}>
-														<Field label="Item name" w={"35%"}>
-															<Input
-															onChange={(e) => handleInputChange(item.id, "itemName", e.target.value)}
-															/>
-
-														</Field>
-														<Field label="Quantity" w={"35%"}>
-															<Input 
-															onChange={(e) => handleInputChange(item.id, "quantity", e.target.value)}/>
-
-														</Field>
-														<Box w={"25%"}>
-
-														{(item.itemName !== "" && item.quantity !== "")  &&
-															<Button onClick={() => handleAddItem(item.id)}>
-																Add item
-															</Button>
-														}
-														
-														</Box>
-														
-													</Flex>
-													))}
-													
+														<Button
+															borderRadius="full"
+															p={0}
+															onClick={() => {
+																handleAddInputFields();
+															}}
+														>
+															<CirclePlus />
+														</Button>
+														{inputs?.itemList?.map((item) => (
+															<Flex gap={4} alignItems={"end"} key={item.id}>
+																<Field label="Item name" w={"35%"}>
+																	<Input
+																		onChange={(e) =>
+																			handleInputChange(
+																				item.id,
+																				"itemName",
+																				e.target.value
+																			)
+																		}
+																	/>
+																</Field>
+																<Field label="Quantity" w={"35%"}>
+																	<Input
+																		onChange={(e) =>
+																			handleInputChange(
+																				item.id,
+																				"quantity",
+																				e.target.value
+																			)
+																		}
+																	/>
+																</Field>
+																<Box w={"25%"}>
+																	{item.itemName !== "" &&
+																		item.quantity !== "" && (
+																			<Button
+																				onClick={() => handleAddItem(item.id)}
+																			>
+																				Add item
+																			</Button>
+																		)}
+																</Box>
+															</Flex>
+														))}
 													</Flex>
 												</Collapsible.Content>
 											</Flex>
 										</Collapsible.Root>
-
 									</Flex>
 								</DialogBody>
 								<DialogFooter>
@@ -372,7 +414,11 @@ const App = () => {
 										<Button variant="outline">Cancel</Button>
 									</DialogActionTrigger>
 									<DialogActionTrigger asChild>
-										<Button onClick={() => handleAddShoppingList(inputs,itemsFromInputs)}>
+										<Button
+											onClick={() =>
+												handleAddShoppingList(inputs, itemsFromInputs)
+											}
+										>
 											Add Shopping List
 										</Button>
 									</DialogActionTrigger>
@@ -383,61 +429,83 @@ const App = () => {
 					</Flex>
 				</Flex>
 				{shoppingList
-				?.filter(
-					(item) =>
-					// User must be a member or the owner
-					(item.memberList.includes(loggedUser?.id) || item.owner === loggedUser?.id) &&
-					// Archived lists are shown only to owners of archived lists if showArchived is true
-					((!item.isArchived && !showArchived) || (item.isArchived && loggedUser?.id === item.owner && showArchived))
-				)
-				.map((item) => (
-					<Box key={item.id} border={"1px solid black"} px={"32px"} py={"32px"}>
-					<Flex justifyContent={"space-between"} alignItems={"center"}>
-						<Flex gap={4} alignItems={"center"}>
-						<Checkbox size="md" colorPalette="green" checked={item.isDone} />
-						<Text>{item.name}</Text>
-						</Flex>
-						<Flex gap={4} alignItems={"center"}>
-						<Button
-							onClick={() => handleShowListDetail(item.id)}
-							variant="ghost"
-							p={0}
-							m={0}
+					?.filter(
+						(item) =>
+							// User must be a member or the owner
+							(item.memberList.includes(loggedUser?.id) ||
+								item.owner === loggedUser?.id) &&
+							// Archived lists are shown only to owners of archived lists if showArchived is true
+							((!item.isArchived && !showArchived) ||
+								(item.isArchived &&
+									loggedUser?.id === item.owner &&
+									showArchived))
+					)
+					.map((item) => (
+						<Box
+							key={item.id}
+							color={"text"}
+							px={"32px"}
+							py={"32px"}
+							borderRadius={"8px"}
+							bgColor={{ base: "gray.100", _dark: "gray.800" }}
 						>
-							<Image w="32px" h="32px" objectFit="cover" src={showIcon} />
-						</Button>
-						{loggedUser && (
-							<>
-							<Image
-								w="36px"
-								h="32px"
-								display={loggedUser?.id === item.owner ? "block" : "none"}
-								objectFit="cover"
-								cursor="pointer"
-								src={saveIcon}
-								bg={item.isArchived ? "green.500" : "none"}
-								onClick={() => {
-								handleArchiveList(item.id);
-								}}
-								borderRadius={"full"}
-							/>
-							<Image
-								w="30px"
-								h="30px"
-								display={loggedUser?.id === item.owner ? "block" : "none"}
-								objectFit="cover"
-								cursor="pointer"
-								src={deleteIcon}
-								onClick={() => {
-								handleDeleteList(item.id);
-								}}
-							/>
-							</>
-						)}
-						</Flex>
-					</Flex>
-					</Box>
-				))}
+							<Flex justifyContent={"space-between"} alignItems={"center"}>
+								<Flex gap={4} alignItems={"center"}>
+									<Checkbox
+										size="md"
+										colorPalette="green"
+										checked={item.isDone}
+									/>
+									<Text>{item.name}</Text>
+								</Flex>
+								<Flex gap={4} alignItems={"center"}>
+									<Box
+										onClick={() => handleShowListDetail(item.id)}
+										variant="ghost"
+										p={0}
+										m={0}
+										color={{ base: "black", _dark: "white" }}
+										cursor="pointer"
+									>
+										<Eye />
+									</Box>
+									{loggedUser && (
+										<>
+											<Box
+												display={
+													loggedUser?.id === item.owner ? "block" : "none"
+												}
+												cursor="pointer"
+												p={"2px"}
+												color={{ base: "black", _dark: "white" }}
+												onClick={() => {
+													handleArchiveList(item.id);
+												}}
+												borderRadius={"full"}
+											>
+												<Bookmark
+													fill={item.isArchived ? "#90ee90" : "transparent"}
+													stroke={item.isArchived ? "green.500" : "white"}
+												/>
+											</Box>
+											<Box
+												display={
+													loggedUser?.id === item.owner ? "block" : "none"
+												}
+												color={{ base: "red", _dark: "red" }}
+												cursor="pointer"
+												onClick={() => {
+													handleDeleteList(item.id);
+												}}
+											>
+												<Trash2 />
+											</Box>
+										</>
+									)}
+								</Flex>
+							</Flex>
+						</Box>
+					))}
 			</Flex>
 		</>
 	);
