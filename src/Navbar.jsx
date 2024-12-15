d4067944ef006821f3ec16df97202800fd798bc8
Vojta Cvrcheck <vojta.cvrcek1@gmail.com>
2024-12-15 14:15:19 +0100

    Added functionality to languge switcher

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -29,18 +29,25 @@ import {
 	DrawerTitle,
 	DrawerTrigger,
 } from "./components/ui/drawer";
-import englishFlag from "./assets/icons/english-flag-icon.svg"
-import czechFlag from "./assets/icons/czech-republic-flag-icon.svg"
+import englishFlag from "./assets/icons/english-flag-icon.svg";
+import czechFlag from "./assets/icons/czech-republic-flag-icon.svg";
 import {
-  MenuContent,
-  MenuItem,
-  MenuItemCommand,
-  MenuRoot,
-  MenuTrigger,
-} from "./components/ui/menu"
+	MenuContent,
+	MenuItem,
+	MenuItemCommand,
+	MenuRoot,
+	MenuTrigger,
+} from "./components/ui/menu";
+import { useTranslation } from "react-i18next";
 
 const Navbar = () => {
 	const [open, setOpen] = useState(false);
+	const lngs = {
+		en: { nativeName: "English", flag: englishFlag },
+		cz: { nativeName: "Czech", flag: czechFlag },
+	};
+
+	const { t, i18n } = useTranslation();
 	return (
 		<Box
 			px={{ base: "32px", sm: "48px" }}
@@ -58,26 +65,46 @@ const Navbar = () => {
 					alignItems={"center"}
 					height={"32px"}
 				>
-				<MenuRoot>
-					<MenuTrigger asChild>
-						<Button variant="outline" size="xs">
-						LANG
-						</Button>
-					</MenuTrigger>
-					<MenuContent minWidth={"55px"}>
-						<Flex justifyConten="center" flexDir="column" alignItems={"center"}>
-							<MenuItem value="new-txt-a" >
-							ENG <Image src={englishFlag} width={"25px"}/>
-							</MenuItem>
-
-							<MenuItem value="new-txt-">
-							CZE <Image src={czechFlag} width={"25px"}/>
-							</MenuItem>
-						</Flex>
-					</MenuContent>
-				</MenuRoot>
-				<ColorModeButton borderRadius={"full"} />
-				<Avatar size="xl" name="Sage" src="https://bit.ly/sage-adebayo" />
+					<MenuRoot>
+						<MenuTrigger asChild>
+							<Button variant="outline" size="xs">
+								LANG
+							</Button>
+						</MenuTrigger>
+						<MenuContent minWidth={"55px"}>
+							<Flex
+								justifyConten="center"
+								flexDir="column"
+								alignItems={"center"}
+							>
+								{Object.keys(lngs).map((lng, index) => (
+									<Flex
+										key={index}
+										value="new-txt-a"
+										type="submit"
+										onClick={() => i18n.changeLanguage(lng)}
+										fontWeight={
+											i18n.resolvedLanguage === lng ? "bold" : "normal"
+										}
+										backgroundColor={
+											i18n.resolvedLanguage === lng ? "green.500" : "default"
+										}
+										justifyContent={"center"}
+										alignItems={"center"}
+										width={"auto"}
+										px={"20px"}
+										py={"12px"}
+										gap={"8px"}
+									>
+										{lngs[lng].nativeName}
+										<Image src={lngs[lng].flag} width={"25px"} />
+									</Flex>
+								))}
+							</Flex>
+						</MenuContent>
+					</MenuRoot>
+					<ColorModeButton borderRadius={"full"} />
+					<Avatar size="xl" name="Sage" src="https://bit.ly/sage-adebayo" />
 				</Flex>
 				<DrawerRoot open={open} onOpenChange={(e) => setOpen(e.open)}>
 					<DrawerBackdrop />
@@ -87,19 +114,28 @@ const Navbar = () => {
 						</Box>
 					</DrawerTrigger>
 					<DrawerContent>
-						<DrawerHeader>
-						</DrawerHeader>
+						<DrawerHeader></DrawerHeader>
 						<DrawerBody p="0">
-						<VStack justifyContent={"start"} alignItems={"start"} w="100%">
-							<HStack borderBottom="1px solid white" w="100%" p="16px" justifyContent={"space-between"}>
-								<Text>Appearance</Text>
-								<ColorModeButton borderRadius={"full"} />
-							</HStack>
-							<HStack borderBottom="1px solid white" w="100%" p="16px" justifyContent={"space-between"}>
-								<Text>Language</Text>
-								Language icon
-							</HStack>
-						</VStack>
+							<VStack justifyContent={"start"} alignItems={"start"} w="100%">
+								<HStack
+									borderBottom="1px solid white"
+									w="100%"
+									p="16px"
+									justifyContent={"space-between"}
+								>
+									<Text>Appearance</Text>
+									<ColorModeButton borderRadius={"full"} />
+								</HStack>
+								<HStack
+									borderBottom="1px solid white"
+									w="100%"
+									p="16px"
+									justifyContent={"space-between"}
+								>
+									<Text>Language</Text>
+									Language icon
+								</HStack>
+							</VStack>
 						</DrawerBody>
 						<DrawerCloseTrigger />
 					</DrawerContent>
